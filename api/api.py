3ecd70f8f945c3b1af2c53e8686bd4f386ce5f9c
monjerry <gerardo.montoya.v@gmail.com>
2017-01-29 20:33:48 -0600

    First draft of API.
    * Added search endpoint with validator
    * Added healthcheck

--- api/api.py
+++ api/api.py
@@ -1,7 +1,43 @@
-from bottle import route, run, template
+from bottle import abort, HTTPResponse, request, route, run
+import requests
 
-@route('/')
+from cfg.dev import *
+
+
+def search_validator(func):
+   def func_wrapper(filters, **kwargs):
+        if request.query.query:
+            kwargs['query'] = request.query.query
+        else:
+            return HTTPResponse(status=400, body={'status': 400, 'msg': 'You need to add the query param'})
+        if not filters in search_filters:
+            return HTTPResponse(status=400, body={'status': 400, 'msg': 'Valid filters are {}'.format(', '.join(search_filters))})
+        if filters == 'all':
+            filters = search_filters[:-1]
+        return func(filters, **kwargs)
+   return func_wrapper
+
+@route('/healthcheck')
 def index():
     return {'healthcheck': 200}
 
-run(host='localhost', port=8080, reloader=True)
+
+@route('/search/<filters>')
+@search_validator
+def search(filters, query=None):
+
+    payload = {
+        'q': 'name:{}'.format(query),
+        'type': filters
+    }
+    try:
+        response = requests.get(spotify_url + 'search', params=payload)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            body = {'status': response.status_code, 'msg': 'Unexpected error: {}'.format(response)}
+            return HTTPResponse(status=response.status_code, body=body)
+    except requests.exceptions.RequestException as e:
+        return HTTPResponse(status=500, body={'error': '{}'.format(e)})
+
+run(host='localhost', port=8080)
---
+++ api/cfg/dev.py
@@ -0,0 +1,2 @@
+spotify_url = 'https://api.spotify.com/v1/'
+search_filters = ['album', 'artist', 'playlist', 'track', 'all']
